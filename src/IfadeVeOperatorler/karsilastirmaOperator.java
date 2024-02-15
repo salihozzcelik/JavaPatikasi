8fca474d17e5275b508e0ac8dbd17edea8b07163
salihozzcelik@gmail.com <salihozzcelik@gmail.com>
2024-02-16 01:53:32 +0300

    karsilastirma operator ders 1

---
+++ src/IfadeVeOperatorler/karsilastirmaOperator.java
@@ -0,0 +1,20 @@
+package IfadeVeOperatorler;
+
+public class karsilastirmaOperator {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 20;
+        int C = 30;
+        int D = 40;
+
+        System.out.println(A == B); // eşittir
+        System.out.println(A != C);
+        System.out.println(B > D);
+        System.out.println(C < A);
+        System.out.println(B >= D);
+
+
+    }
+
+
+}
