896de95e9921e766723b1cf16f6f7fa001d1ea63
salihozzcelik@gmail.com <salihozzcelik@gmail.com>
2024-02-17 04:15:25 +0300

    operatorlers

---
+++ src/IfadeVeOperatorler/karsilastirmaOperator2.java
@@ -0,0 +1,12 @@
+package IfadeVeOperatorler;
+
+public class karsilastirmaOperator2 {
+
+    public static void main(String[] args) {
+        int a = 10, b =20, c;
+        c = (a == b) ? 2 : 3;
+        System.out.println(c);
+
+
+    }
+}
---
+++ src/IfadeVeOperatorler/mantiksalOperator.java
@@ -0,0 +1,17 @@
+package IfadeVeOperatorler;
+
+public class mantiksalOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 5;
+
+        boolean kosul1 = (a < b);
+        boolean kosul2 = (a > c);
+        boolean sonuc = kosul1 && kosul2;
+
+        System.out.println(!sonuc);
+
+
+    }
+
+
+}
