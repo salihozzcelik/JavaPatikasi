f655e25c49b37bc9f7ac187d1f75558a1c3278d3
salihozzcelik@gmail.com <salihozzcelik@gmail.com>
2024-02-19 01:55:12 +0300

    operatorlers

---
+++ src/IfadeVeOperatorler/elseif.java
@@ -0,0 +1,21 @@
+package IfadeVeOperatorler;
+
+public class elseif {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 1;
+
+        if (a < b && a < c) {
+            System.out.println("a en küçüktür");
+
+
+        } else if (b < a && b < c) {
+            System.out.println("b en küçük sayıdır");
+        } else  {
+            System.out.println("c en küçüktür");
+        }
+
+
+    }
+
+
+}
---
+++ src/IfadeVeOperatorler/kodBloklarindanifveElse.java
@@ -0,0 +1,24 @@
+package IfadeVeOperatorler;
+
+public class kodBloklarindanifveElse {
+    public static void main(String[] args) {
+        int a = 20, b = 20;
+        boolean compare = a == b;
+        // String str = compare ? "eşittir" : "eşit değildir";
+        //System.out.println(str);
+
+
+        if (compare) {
+            a = 50;
+            System.out.println("a sayısı" + a);
+            System.out.println("eşittir");
+
+        } else {
+            System.out.println("eşit değildir");
+        }
+
+
+    }
+
+
+}
