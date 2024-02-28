72d3fa9292bb20705faa020ca5006b37d44e8b5a
salihozzcelik@gmail.com <salihozzcelik@gmail.com>
2024-02-29 00:08:40 +0300

    donguler

---
+++ src/Donguler/forDongusu.java
@@ -0,0 +1,14 @@
+package Donguler;
+
+public class forDongusu {
+    public static void main(String[] args) {
+
+        // kac kere doncekse o zaman kullanıyoruz
+        for (int sayac = 1; sayac <= 10; sayac++) {
+
+            System.out.println(sayac);
+        }
+
+
+    }
+}
